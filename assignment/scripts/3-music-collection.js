a0a2215d640f320cc1e218646450ed90ec4699d4
Laokeng Vang <laokengv@gmail.com>
2023-04-23 20:01:46 -0500

    Second commit. Console logged 6 albums. Also, added the showCollection function.

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -12,3 +12,22 @@ let albums = {
   return albums;
 }
 
+console.log('Adding first album:', addToCollection('Social Cues', 'Cage The Elephant', '2019'));
+console.log('Adding second album:', addToCollection('KOD', 'J. Cole', '2018'));
+console.log('Adding third album:', addToCollection('In Return', 'Odesza', '2014'));
+console.log('Adding fourth album:', addToCollection('Circles', 'Mac Miller', '2020'));
+console.log('Adding fifth album:', addToCollection('On Top of The Covers', 'T-Pain', '2023'));
+console.log('Adding sixth album:', addToCollection('Goblin', 'Tyler the Creator', '2011'));
+
+console.log(collection);
+
+function showCollection(array){
+    console.log('Number of songs in the collecton: ${array.length}');
+    for(let i of array){
+      console.log('${array[i].title} by ${array[i].artist}, published in ${array[i].yearPublished}');
+    }
+  }
+  
+  // console.log(showCollection(collection));
+  
+  
